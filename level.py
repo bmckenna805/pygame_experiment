6ff234935a64aeaee41cc134517a8a6965eccc8e
Ben McKenna <air805ronin@hotmail.com>
2021-01-04 20:43:44 -0600

    Updates to tutorial to reflect the direction I want to go

--- level.py
+++ level.py
@@ -1,6 +1,6 @@
 import configparser
 import pygame
-import load_tiles 
+import tileset
 
 class Level(object):
     def load_file(self, filename="map.default"):
@@ -26,7 +26,7 @@ class Level(object):
             return self.key[char]
         except KeyError:
             return {}
-        
+
     def get_bool(self, x, y, name):
         """Tell if the specified flag is set for position on the map."""
 
@@ -53,43 +53,19 @@ class Level(object):
         for map_y, line in enumerate(self.map):
             for map_x, c in enumerate(line):
                 if wall(map_x, map_y):
-                    # Draw different tiles depending on neighbourhood
-                    if not wall(map_x, map_y+1):
-                        if wall(map_x+1, map_y) and wall(map_x-1, map_y):
-                            tile = 1, 2
-                        elif wall(map_x+1, map_y):
-                            tile = 0, 2
-                        elif wall(map_x-1, map_y):
-                            tile = 2, 2
-                        else:
-                            tile = 3, 2
-                    else:
-                        if wall(map_x+1, map_y+1) and wall(map_x-1, map_y+1):
-                            tile = 1, 1
-                        elif wall(map_x+1, map_y+1):
-                            tile = 0, 1
-                        elif wall(map_x-1, map_y+1):
-                            tile = 2, 1
-                        else:
-                            tile = 3, 1
-                    # Add overlays if the wall may be obscuring something
-                    if not wall(map_x, map_y-1):
-                        if wall(map_x+1, map_y) and wall(map_x-1, map_y):
-                            over = 1, 0
-                        elif wall(map_x+1, map_y):
-                            over = 0, 0
-                        elif wall(map_x-1, map_y):
-                            over = 2, 0
-                        else:
-                            over = 3, 0
-                        overlays[(map_x, map_y)] = tiles[over[0]][over[1]]
+                    try:
+                        tile = self.key[c]['tile'].split(',')
+                        tile = int(tile[0]), int(tile[1])
+                    except (ValueError, KeyError):
+                        # Default to wall tile
+                        tile = 6, 0
                 else:
                     try:
                         tile = self.key[c]['tile'].split(',')
                         tile = int(tile[0]), int(tile[1])
                     except (ValueError, KeyError):
                         # Default to ground tile
-                        tile = 0, 3
+                        tile = 3, 0
                 tile_image = tiles[tile[0]][tile[1]]
                 image.blit(tile_image,
                            (map_x*MAP_TILE_WIDTH, map_y*MAP_TILE_HEIGHT))
@@ -101,7 +77,7 @@ if __name__ == "__main__":
     MAP_TILE_WIDTH = 32
     MAP_TILE_HEIGHT = 32
     MAP_CACHE = {
-        'tileset.png': load_tiles.load_tile_table('tileset.png', MAP_TILE_WIDTH,
+        'tileset.png': tileset.load_tile_table('tileset.png', MAP_TILE_WIDTH,
                                       MAP_TILE_HEIGHT),
     }
 
